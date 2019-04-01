96f64e89d4ad273ec1424b1a2c28d82b34185bf9
Christoffer Cajander <christoffer.bjorn.cajander@gmail.com>
2019-04-01 18:21:07 +0200

    Added list view

--- scripts/controllers/page.controller.js
+++ scripts/controllers/page.controller.js
@@ -1,4 +1,6 @@
 app.controller('pageController', function($scope, $rootScope) {
 
+    
     $rootScope.rowLimits = 10;
+    $scope.totalItems = products.length;
 })
--- scripts/controllers/sort.controller.js
+++ scripts/controllers/sort.controller.js
@@ -1,7 +1,7 @@
 app.controller('sortController', function($scope, $rootScope) {
     $rootScope.sortColumn = "productname";
     $rootScope.reverseSort = false;
-    
+
     $rootScope.sortData = function(column) {
         $rootScope.reverseSort = ($scope.sortColumn == column) ?  !$scope.reverseSort : false;
         $rootScope.sortColumn = column;
