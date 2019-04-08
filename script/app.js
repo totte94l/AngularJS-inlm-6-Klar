881c3c5aebaec7b0e0ca67e203a4362e145f3cef
Christoffer Cajander <christoffer.bjorn.cajander@gmail.com>
2019-04-08 17:49:09 +0200

    Added sorting functionality

--- script/app.js
+++ script/app.js
@@ -19,6 +19,18 @@ var app = angular
         $scope.productsPerPage = 10; // Sets the default value for products per page.
         $scope.totalProducts = $scope.products.length;
 
+        /* Sorting */
+        $scope.sortColumn = "productname"; //Default sorting type
+        $scope.reverseSort = false;
+
+        $scope.sortData = function(column) {
+            $scope.reverseSort = ( $scope.sortColumn == column ) ? !$scope.reverseSort : false;
+            $scope.sortColumn = column;
+        }
+
+        /* View icon (card or list) */
+        $scope.cardActive = true; 
+
     })
 
 
