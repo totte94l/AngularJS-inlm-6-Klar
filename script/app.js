7e8d434cf4465eea787f2d2b9d8e2ba4bb003701
Christoffer Cajander <christoffer.bjorn.cajander@gmail.com>
2019-04-08 17:37:55 +0200

    Added search and rowlimits functions

--- script/app.js
+++ script/app.js
@@ -2,17 +2,23 @@ var app = angular
     .module('shopApp', ['ngRoute'])
     .config(function($routeProvider) {
         $routeProvider.when("/", {
-            templateUrl : "partials/card-view/card.view.html",
-            controller: "mainController"
+            templateUrl : "partials/card-view/card.view.html"
+            // controller: "mainController"
         })
         .when("/list-view", {
-            templateUrl: "partials/list-view/list.view.html",
-            controller: "mainController"
+            templateUrl: "partials/list-view/list.view.html"
+           // controller: "mainController"
         })
-    });
+    })
 
-    app.controller('mainController', function($scope) {
-        
+   .controller('mainController', function($scope) {
+        // Adds all products to the scope from products.js
+        $scope.products = products;
+
+        /* Products per page values */
+        $scope.productsPerPage = 10; // Sets the default value for products per page.
+        $scope.totalProducts = $scope.products.length;
+
     })
 
 
